51752fe3278638476f42f9028f70e6583f755c23
wulihua <wulihua0@gmail.com>
2013-12-13 23:54:52 +0800

    structure modification

--- bin/client.py
+++ bin/client.py
@@ -1,23 +1,125 @@
+#!/usr/bin/python
+#coding:utf-8
+
+import urllib
+import popen2
+import re
 import logging, logging.handlers
+	
+def test_popen():
+	r = popen2.Popen4("cat grepText.py |grep test")
+	print r.poll()
+	s = r.wait()
+	print s
+	
+	s = r.fromchild.readlines()
+	for i in s:
+		print i.strip()
 
-rootLogger = logging.getLogger('')
-rootLogger.setLevel(logging.DEBUG)
-socketHandler = logging.handlers.SocketHandler('localhost',
-                    logging.handlers.DEFAULT_TCP_LOGGING_PORT)
-# don't bother with a formatter, since a socket handler sends the event as
-# an unformatted pickle
-rootLogger.addHandler(socketHandler)
+class GrepText:
+	def grepFile(self, cmd):
+		r = popen2.Popen4(cmd)
+		# r.wait() returns the status code of wait()
+		s = r.fromchild.readlines()
+		return [i.strip() for i in s]
 
-# Now, we can log to the root logger, or any other logger. First the root...
-logging.info('Jackdaws love my big sphinx of quartz.')
+	def grepLine(self, line):
+		#line example
+		# s="NOTICE: 12-11 15:45:15:  newsdiproxy. * 2007337312 [work_thread_func]: send newsdi of url[http://www.chinanews.com/gn/2013/11-12/5489300.shtml?f=baidu] ok,error no[0] all time use[1] 			       	process time use[1] queue wait time [0] queue len is[0] re_crawl_page_times [0]"
 
-# Now, define a couple of other loggers which might represent areas in your
-# application:
+		# and the list will be:
+		#funcname, url, error_no, all_time_use, process_time, queue_wait_time, queue_len, re_crawl_times(this will always be 0)
+		# and the result will be like:
+		#['[work_thread_func]', '[http://www.chinanews.com/gn/2013/11-12/5489300.shtml?f=baidu]', '[0]', '[1]', '[1]', '[0]', '[0]', '[0]']
+		regex = "(\[\S+\])+"	
+		l = re.findall(regex, line)
+		#strip the square braces
+		l = [i.lstrip('[').rstrip(']') for i in l]
 
-logger1 = logging.getLogger('myapp.area1')
-logger2 = logging.getLogger('myapp.area2')
+		keys = ['func', 'url', 'error_no', 'all_time', 'process_time',
+			'queue_wait_time', 'queue_len', 're_crawl_times']
+		return dict(zip(keys, l))
 
-logger1.debug('Quick zephyrs blow, vexing daft Jim.')
-logger1.info('How quickly daft jumping zebras vex.')
-logger2.warning('Jail zesty vixen who grabbed pay from quack.')
-logger2.error('The five boxing wizards jump quickly.')
+class Task:
+	def __init__(self):
+		self.setup()
+		try:
+			self.handle()
+		finally:
+			self.finish()
+
+	def setup(self):
+		pass
+
+	def handle(self):
+		pass
+
+	def finish(self):
+		pass
+
+class Extractor(Task):
+	def __init__(self, cmd):
+		self.cmd = cmd
+		self.log_list = list()
+		Task.__init__(self)
+
+	def setup(self):
+		r = popen2.Popen4(self.cmd)
+		# r.wait() returns the status code of wait()
+		s = r.fromchild.readlines()
+		self.lines = [i.strip() for i in s]
+
+	def handle_line(self, line):
+		#line example
+		# s="NOTICE: 12-11 15:45:15:  newsdiproxy. * 2007337312 [work_thread_func]: send newsdi of url[http://www.chinanews.com/gn/2013/11-12/5489300.shtml?f=baidu] ok,error no[0] all time use[1] 			       	process time use[1] queue wait time [0] queue len is[0] re_crawl_page_times [0]"
+
+		# and the list will be:
+		#funcname, url, error_no, all_time_use, process_time, queue_wait_time, queue_len, re_crawl_times(this will always be 0)
+		# and the result will be like:
+		#['[work_thread_func]', '[http://www.chinanews.com/gn/2013/11-12/5489300.shtml?f=baidu]', '[0]', '[1]', '[1]', '[0]', '[0]', '[0]']
+		regex = "(\[\S+\])+"	
+		l = re.findall(regex, line)
+		#strip the square braces
+		l = [i.lstrip('[').rstrip(']') for i in l]
+
+		keys = ['func', 'url', 'error_no', 'all_time', 'process_time',
+			'queue_wait_time', 'queue_len', 're_crawl_times']
+		return dict(zip(keys, l))
+		
+	def handle(self):
+		for line in self.lines:
+			r = self.handle_line(line)
+			self.log_list.append(r)
+
+class Sender(Task):
+	def __init__(self, logs, logger):
+		self.log_list = logs
+		self.logger = logger
+		Task.__init__(self)
+	
+	def handle(self):
+		for i in self.log_list:
+			self.logger.info(i)
+			
+
+
+class Logger:
+	def __init__(self, log_name, host, port = logging.handlers.DEFAULT_TCP_LOGGING_PORT):
+		self.name = log_name
+		self.host = host
+		self.port = port
+		self.setup()
+
+	def setup(self):
+		self.logger = logging.getLogger(self.name)
+		sh = logging.handlers.SocketHandler(self.host, self.port)
+		self.logger.setLevel(logging.INFO)
+		self.logger.addHandler(sh)
+
+def getLogger(log_name, host, 
+				port = logging.handlers.DEFAULT_TCP_LOGGING_PORT): 
+	return Logger(log_name, host, port).logger
+
+			
+if __name__ == '__main__':
+	pass
--- test/testGrepText.py
+++ test/testGrepText.py
@@ -6,34 +6,35 @@ import sys
 if not "../bin/" in sys.path:
 	sys.path.append("../bin/")
 
-import grepText
+import client
+
 
 def test_grepText():
-	b = grepText.GrepText()
+	b = client.GrepText()
 	l = b.grepFile("cat data |grep 'time use'")
 	for i in l:
 		print i
 
 def test_grepLine():
 	s ="NOTICE: 12-11 15:45:15:  newsdiproxy. * 2007337312 [work_thread_func]: send newsdi of url[http://www.chinanews.com/gn/2013/11-12/5489300.shtml?f=baidu] ok,error no[0] all time use[1] 			       	process time use[1] queue wait time [0] queue len is[0] re_crawl_page_times [0]"
-	b = grepText.GrepText()
+	b = client.GrepText()
 	print b.grepLine(s)
 
 def test_extractor():
-	b = grepText.Extractor("cat data.1000 |grep 'time use'")	
+	b = client.Extractor("cat data.1000 |grep 'time use'")	
 	for i in  b.log_list:
 		print i
 
 def test_sender():	
-	b = grepText.Extractor("cat data |grep 'time use'")	
-	logger = grepText.getLogger('will-laptop', 'localhost')
-	s = grepText.Sender(b.log_list, logger)
+	b = client.Extractor("cat data.300 |grep 'time use'")	
+	logger = client.getLogger('will-laptop', 'localhost')
+	s = client.Sender(b.log_list, logger)
 	
 
 if __name__ == '__main__':
 	#test_grepText()
-	#test_grepLine()
+	test_grepLine()
 	#test_extractor()
-	test_sender()
+	#test_sender()
 	pass
 
